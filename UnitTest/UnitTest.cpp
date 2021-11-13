c240f5ebafcb9763e02eeb2ab630e02c7122ff66
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-13 18:33:32 +0200

    1 commit master

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.3_iter/lab6.3_iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 4;
+			int a[n] = { -3,6,8,-10 };
+			int c;
+			c = SumParn(a, n);
+			Assert::AreEqual(4, c);
+		}
+	};
+}
---
+++ lab6.3_iter/lab6.3_iter.cpp
@@ -0,0 +1,73 @@
+﻿// lab6.3_iter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+using namespace std;
+#include <iostream>
+#include <iomanip>
+
+template <typename T>
+void CreateArray(T* a, const int size, const int Low, const int High) {
+	for (int i = 0; i < size; i++) {
+		a[i] = Low + rand() % (High - Low + 1);
+	}
+}
+
+void CreateArray(int* a, const int size, const int Low, const int High) {
+	for (int i = 0; i < size; i++) {
+		a[i] = Low + rand() % (High - Low + 1);
+	}
+}
+template <typename T>
+void PrintArray(T* a, const int size) {
+	cout << " a[" << size << "] = " << "{";
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << a[i];
+	}
+	cout << " }" << endl;
+}
+void PrintArray(int* a, const int size) {
+	cout << " a[" << size << "] = " << "{";
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << a[i];
+	}
+	cout << " }" << endl;
+}
+template <typename T>
+T SumParn(T* a, const int size) {
+	T s = 0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] % 2 == 0) {
+			s += a[i];
+		}
+	}
+	return s;
+}
+int SumParn(int* a, const int size) {
+	int s = 0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] % 2 == 0) {
+			s += a[i];
+		}
+	}
+	return s;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+	const int n=7;
+	int a[n];
+	int Low, High;
+	cout << "Enter low value "; cin >> Low;
+	cout << endl;
+	cout << "Enter high value "; cin >> High;
+	
+	
+	CreateArray<int>(a, n, Low, High); //шабл
+	//CreateArray(a, n, Low, High);
+	PrintArray<int>(a, n);  //шабл
+	//PrintArray(a, n);
+	cout << " SumParn = " << SumParn<int>(a,n)<<endl;  //шабл
+	//cout << " SumParn = " << SumParn(a, n) << endl;
+    return 0;
+}
+
